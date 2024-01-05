ec4e8bc730394a5210c8ffe84594affe903fd512
Johnson Shih <jshih@microsoft.com>
2024-01-04 19:43:55 -0800

    bump to v2

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-docker build -t deviceservice:v1 -f Dockerfile .
+docker build -t deviceservice:v2 -f Dockerfile .
+
+CONTAINER_REPOSITORY=ghcr.io/johnsonshih/akri
+docker login $CONTAINER_REPOSITORY
+docker build -t ghcr.io/johnsonshih/deviceservice:v2 -f Dockerfile .
+docker push ghcr.io/johnsonshih/deviceservice:v2
+
